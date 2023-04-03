c7d5106eaba30c110405d6e1067e668aa03194d2
Elhanah Otasowie <125979140+miss-sunflower@users.noreply.github.com>
2023-04-03 13:57:06 +0100

    re-worked the original code

--- agecalc.js
+++ agecalc.js
@@ -1,4 +1,4 @@
-function getAge() {
+/*function getAge() {
     let err_container = document.querySelector("#err_container");
     err_container.innerHTML = '';
 
@@ -14,4 +14,18 @@ function getAge() {
     
     err_container.innerHTML = `You're currently ${myAge} years old.`;
 
+}*/
+function getAge(){
+let ynumber = parseInt(document.querySelector("#age").value); //parseInt to make sure it's a number
+let currentYear = new Date().getFullYear();
+let some_thing = document.querySelector("#some_thing");
+some_thing.innerHTML = '';
+
+ if (ynumber >= 1000 && ynumber <= 9999){
+    let myAge = currentYear - ynumber;
+    some_thing.innerHTML = `You're currently ${myAge} years old.`;
+} else {
+  some_thing.innerHTML = "The Year Entered is not valid. Try Again!";
 }
+}
+
